c89fc43e7f0039acb30b1daf3b46bb7148f65381
yunxiangnian <songweili0402@qq.com>
2021-03-22 12:22:47 +0800

    Initial commit

---
+++ wholemanager/src/main/java/com/cloud/lsw/WholemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.lsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WholemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WholemanagerApplication.class, args);
+	}
+
+}
